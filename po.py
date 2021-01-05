ec96c3416a330c68a5cdbc915bb04b04a73e4a79
ingli0 <76855285+ingli0@users.noreply.github.com>
2021-01-05 23:44:53 +0200

    good

--- po.py
+++ po.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import random
-
+import io
 client = commands.Bot(command_prefix= '.')
 
 @client.event
@@ -94,7 +94,41 @@ async def _bet(ctx, omada1, *,omada2):
     'under']
     await ctx.send(f'match: {omada1}-{omada2}\nna to paiksis: {random.choice(apan)}') 
 
+@client.command(name="picon", brief="Sends your icon's link")
+async def get_icon(ctx: commands.Context, *, member: discord.Member = None) -> None:
+    """
+    Send the author's icon url to the channel
+    :param member: (Optional) you can pass a member if you want to view this member's icon
+    """
 
+    if not member:
+        url = ctx.author.avatar_url
+        await ctx.send(f"{ctx.author.mention} your icon is located at: {url}")
+    else:
+        url = member.avatar_url
+        await ctx.send(f"{ctx.author.mention}. This member's icon is located at: {url}")
+
+
+@client.command(name="hmm", aliases=["hm", "swirl"], brief="Distorts your icon")
+async def hmm(ctx: commands.Context, *, user: discord.User = None) -> None:
+    """Take's a user's icon and creates a gif swiverling it"""
+
+    # Create a bytes-like object to save the user's avatar on
+    image_file = io.BytesIO()
+
+    if user:
+        await user.avatar_url.save(image_file)
+    else:
+        await ctx.author.avatar_url.save(image_file)
+
+    image_file.seek(0)
+
+    # Get the gif
+    img = await client.helpers.edit_swirl(image_file)
+    img.seek(0)
+
+    file = discord.File(img, filename="icon.gif")
+    await ctx.send(file=file)
 
 
 client.run('Nzk0NjgxNTYyMTYzMDUyNjA1.X--W4A.Ord7g6zPxcmMUqaL8CUD2kRjxPc')
